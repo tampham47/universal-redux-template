de6ba3bd11bf8acd81ef1de52c3b6ee7f6026d25
Tam Pham <tampham47@live.com>
2017-09-10 14:44:22 +0700

    d

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,10 +1,10 @@
 import { combineReducers } from 'redux'
-import questions from 'reducers/questions'
-import questionDetail from 'reducers/questionDetail'
+// import questions from 'reducers/questions'
+// import questionDetail from 'reducers/questionDetail'
 
 const rootReducer = combineReducers({
-  questions,
-  questionDetail
+  // questions,
+  // questionDetail
 })
 
 export default rootReducer
